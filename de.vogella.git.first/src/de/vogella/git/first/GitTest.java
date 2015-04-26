d5eb281faeead6c130b89471f15aa1d6da9a6a4a
dumi <eigidi100@gmail.com>
2015-04-26 16:13:58 +0200

    test de nouveau

--- de.vogella.git.first/src/de/vogella/git/first/GitTest.java
+++ de.vogella.git.first/src/de/vogella/git/first/GitTest.java
@@ -5,6 +5,7 @@ public class GitTest {
 	public static void main(String[] args) {
 		 System.out.println("Git is fun. Version initiale.");
 		 System.out.println("version2");
+		 System.out.println("version3");
 	}
 
 }
