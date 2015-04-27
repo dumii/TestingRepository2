417a75b1ad32393dd19a6c5312c8d58fcfe39aeb
dumi <eigidi100@gmail.com>
2015-04-27 08:22:20 +0200

    tester la branch

--- de.vogella.git.first/src/de/vogella/git/first/GitTest.java
+++ de.vogella.git.first/src/de/vogella/git/first/GitTest.java
@@ -6,6 +6,8 @@ public class GitTest {
 		 System.out.println("Git is fun. Version initiale.");
 		 System.out.println("version2");
 		 System.out.println("version3");
+		 
+		 System.out.println("ceci se trouve dans la branch");
 	}
 
 }
